a7287bc177725f9958fd059a7e8d0432d7abb4dd
chonibe <chonibe@gmail.com>
2026-01-19 18:42:52 +0200

    Fix API function imports to use dist directory

--- api/check.js
+++ api/check.js
@@ -1,8 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = handler;
-const supabaseStorage_1 = require("../governor/api/src/storage/supabaseStorage");
-const handlers_1 = require("../governor/api/src/handlers");
+const supabaseStorage_1 = require("../dist/governor/api/src/storage/supabaseStorage");
+const handlers_1 = require("../dist/governor/api/src/handlers");
 const storage = new supabaseStorage_1.SupabaseStorage(process.env.SUPABASE_URL ?? "", process.env.SUPABASE_SERVICE_ROLE_KEY ?? "");
 async function handler(req, res) {
     if (req.method !== "POST") {
